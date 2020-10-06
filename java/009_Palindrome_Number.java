772205f49a84c45cf9d9d784397f979e1e7379a7
arun-aditya <42905401+arun-aditya@users.noreply.github.com>
2020-10-06 19:03:23 +0530

    Update 009_Palindrome_Number.java (#14)
    * Added a simpler and easy to understand code in java, by @arun-aditya

--- java/009_Palindrome_Number.java
+++ java/009_Palindrome_Number.java
@@ -39,3 +39,25 @@ class Solution {
         return true;
     }
 }
+
+// simpler code 
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int r,s=0,number=x;
+        if(number<0){
+            return false;
+        }
+        while (number!=0){
+            r=number%10;
+            s= s*10 +r;
+            number/=10;
+        }
+        if (s==x){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
