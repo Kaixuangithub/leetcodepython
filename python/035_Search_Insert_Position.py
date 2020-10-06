29c53b5cc09b19a7ef1d51396ecf3d7637989692
peterhuang1kimo <peterhuang1kimo@gmail.com>
2020-10-06 20:10:09 +0800

    035_Search_Insert_Position Python 3 Support (#27)
    * Update 035_Search_Insert_Position.py for Python 3 support, by @peterhuang1kimo

--- python/035_Search_Insert_Position.py
+++ python/035_Search_Insert_Position.py
@@ -23,13 +23,21 @@ class Solution:
     #     return pos
 
     def searchInsert(self, nums, target):
-        l, r = 0, len(nums) - 1
+        l, r = int(0), len(nums) - 1
         while l < r:
-            mid = (l + r) / 2
+            mid = int((l + r) / 2)
             if nums[mid] < target:
                 l = mid + 1
             else:
                 r = mid
         if nums[l] < target:
             return l + 1
-        return l
+        return l 
+
+    
+    
+if __name__ == '__main__':
+    # begin
+    s = Solution()
+    print (s.searchInsert([1,3,5,6],5))    
+    
